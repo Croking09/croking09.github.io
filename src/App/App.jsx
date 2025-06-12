0fd33a5455972dd27659c5bf4d6d7ada7d0f2054
Javier HM <javierhm2004@gmail.com>
2025-06-12 23:38:53 +0200

    Fixed import

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,4 +1,4 @@
-import SmartNavigator from './ScrollNavigator.jsx'
+import SmartNavigator from './SmartNavigator.jsx'
 import Header from './Header.jsx'
 import Body from './Body.jsx'
 import Footer from './Footer.jsx'
