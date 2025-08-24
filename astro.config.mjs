67d4e0acc9004864893ecb9de60659f125faf6f2
Javier HM <javierhm2004@gmail.com>
2025-08-24 10:57:22 +0200

    Started astro project

---
+++ astro.config.mjs
@@ -0,0 +1,12 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+import path from "node:path";
+
+import tailwindcss from "@tailwindcss/vite";
+
+// https://astro.build/config
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()]
+  }
+});
