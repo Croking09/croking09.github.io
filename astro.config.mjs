cc991f48a53a27f15e52ee31978f699622ee172b
Javier HM <javierhm2004@gmail.com>
2025-08-25 13:29:13 +0200

    SEO boost

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-import path from "node:path";
+import sitemap from "astro-sitemap";
 
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://croking09.github.io",
+  integrations: [sitemap()],
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 });
